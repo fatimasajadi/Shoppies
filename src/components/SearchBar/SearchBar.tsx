3dcf2da698d1fe64dcd767ce3ce8fce6e9e38f4f
fsajadi <s.sfatemeh@gmail.com>
2020-09-07 15:37:08 -0400

    Add search bar

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -5,13 +5,15 @@ import "./SearchBar.css";
 interface Props {
   value: string;
   onChange(value: string): void;
+  isInitial: boolean;
 }
 
-function SearchBar({ value, onChange }: Props) {
+function SearchBar({ value, onChange, isInitial }: Props) {
   return (
-    <div className="search-bar">
+    <div className={`search-bar ${isInitial ? "initial" : ""}`}>
       <input
-        placeholder="Movie title..."
+        placeholder="Start typing your favorite movie name..."
+        autoFocus
         value={value}
         onChange={(event) => {
           onChange(event.target.value);
