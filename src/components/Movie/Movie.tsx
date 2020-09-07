0a704d485ce1abfcb6b670c5404a0952a98e79e0
fsajadi <s.sfatemeh@gmail.com>
2020-09-07 15:39:11 -0400

    Add movie component and some styles

--- src/components/Movie/Movie.tsx
+++ src/components/Movie/Movie.tsx
@@ -1,21 +1,40 @@
 import React from "react";
 import { Movie as IMovie } from "../../utils/api";
+import AddButton from "../AddButton/AddButton";
+import placholder from "../../assets/movie_placeholder.png";
 
 import "./Movie.css";
 
 interface Props {
   movie: IMovie;
+  added: boolean;
+  onAdd(movie: IMovie): void;
+  onRemove(id: string): void;
+  maxedOut: boolean;
 }
 
-function Movie({ movie }: Props) {
+function Movie({ movie, added, onAdd, onRemove }: Props) {
+  const image =
+    !movie.Poster || movie.Poster === "N/A" ? placholder : movie.Poster;
+
   return (
     <div className="movie">
       <div className="poster">
-        <img src={movie.Poster} alt={movie.Title} />
+        <img src={image} alt={movie.Title} />
       </div>
       <div className="title">{movie.Title}</div>
       <div className="year">{movie.Year}</div>
       <div className="type">{movie.Type}</div>
+      <AddButton
+        onClick={() => {
+          if (added) {
+            onRemove(movie.imdbID);
+          } else {
+            onAdd(movie);
+          }
+        }}
+        added={added}
+      />
     </div>
   );
 }
